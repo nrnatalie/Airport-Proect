3856d332c74301828d433dd847b5552fca1be94c
Nathalie Russo <johndoe@example.com>
2023-06-22 12:09:51 +0300

    feat: add method List<Passenger> readFile()

--- src/PassengerList.java
+++ src/PassengerList.java
@@ -1,8 +1,10 @@
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class PassengerList {
 
@@ -28,6 +30,32 @@ public class PassengerList {
       outputFileWriter.write(passenger + "\n");
     }
     outputFileWriter.close();
+  }
+
+  /**
+   * Чтение из файла
+   *
+   * @return result, закрываем файл inputReader.close();
+   * @throws IOException, Возможно исключение при чтении из файла
+   */
+  public List<Passenger> readFile() throws IOException {
+    File input = new File("res/input.txt");
+    FileReader inputReader = new FileReader(input);
+    List<Passenger> result = new ArrayList<>();
+    if (!file.exists() || !file.canRead()) {
+      return result;
+    }
+    Scanner scanner = new Scanner(file);
+    while (scanner.hasNext()) {
+      String line = scanner.nextLine();
+      Passenger passenger = Passenger.parseFrom(line, sep);
+      result.add(passenger);
+
+    }
+    scanner.close();
+    inputReader.close();
+    return result;
+
   }
 
 }
