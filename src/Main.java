4fe24edc3c67aeafbc42f0e4c0e6041e72ce0d46
Nathalie Russo <johndoe@example.com>
2023-06-22 09:52:24 +0300

    feat: launched entry point main and BufferedReader

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,33 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    // Цель -- заказ билета
+    System.out.println(" ========== ЗАКАЗ БИЛЕТА В АЭРОПОРТУ \"BERLIN\" ============");
+    System.out.println();
+    System.out.println();
+
+    List<String> lines = new ArrayList<>();
+    lines.add("LONDON");
+    lines.add("PARIS");
+    lines.add("NEW YORK");
+    lines.add("TOKYO");
+    lines.add("DUBAI");
+    lines.add("MUNICH");
+    lines.add("CHINA");
+    lines.add("INDONESIA");
+    lines.add("INDIA");
+
+    System.out.println(lines);
+    System.out.println(" ");
+    lines.add("BERLIN");
+    System.out.println(lines);
+    System.out.println();
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
   }
 }
