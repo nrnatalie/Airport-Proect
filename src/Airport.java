98efd30a3320b076309fe7971f160ae9c61d0337
Nathalie Russo <johndoe@example.com>
2023-06-22 11:48:14 +0300

    feat: add class Airport - printMenuAirport()

---
+++ src/Airport.java
@@ -0,0 +1,18 @@
+public class Airport {
+  /**
+   * Вывод меню на экран
+   */
+  public static void printMenuAirport() {
+    System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    System.out.println("Ожидание регистрации:");
+    System.out.println("1 - Добавить:");
+    System.out.println("2 - Вывести на экран:");
+    System.out.println("3 - Поиск пассажира:");
+    System.out.println("4 - Запись в файл:");
+    System.out.println("5 - Удалить пассажира:");
+    System.out.println("6 - Сортировать пассажира:");
+    System.out.println("7 - Выход:");
+    System.out.println("Введите команду: ");
+  }
+
+}
